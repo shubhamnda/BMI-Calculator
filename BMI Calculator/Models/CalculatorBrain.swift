e43b3a42da3b5f9b49ef4d8de915ad58700fea66
shubham nanda <shubhamn.ec.22@gmail.com>
2023-09-23 16:53:02 +0530

    BMI calculator

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -33,7 +33,7 @@ struct CalculatorBrain {
         } else if bmiValue < 24.9 {
             bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
         } else {
-            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .red)
         }
     }
 }
